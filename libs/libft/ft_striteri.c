30390932a59538b682bde2596f3d19bd5487f42d
=Facinet Kouyate <=fakouyat@student.42wolfsburg.de>
2022-08-17 19:43:05 +0200

    check_input : partially done

---
+++ libs/libft/ft_striteri.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fakouyat <fakouyat@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 22:50:52 by fakouyat          #+#    #+#             */
+/*   Updated: 2022/06/24 22:01:33 by fakouyat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int	i;
+	int	length;
+
+	if (!s)
+		return ;
+	i = 0;
+	length = ft_strlen(s);
+	while (i < length)
+	{
+		(*f)(i, &s[i]);
+		i++;
+	}
+}
